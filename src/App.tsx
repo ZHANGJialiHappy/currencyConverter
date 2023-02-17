0d1656e1477cc5f615984541cdd114aa7998b5d5
Jiali Zhang <zhangjiali86@gmail.com>
2023-02-17 13:56:41 +0100

    feat: handleamount+useEffectCurrency

--- src/App.tsx
+++ src/App.tsx
@@ -7,26 +7,70 @@ function App() {
   const [currencyOptions, setCurrencyOptions] = useState<string[]>([])
   const [fromCurrency, setFromCurrency] = useState<string>("")
   const [toCurrency, setToCurrency] = useState<string>("")
+  const [exchangeRate, setExchangeRate] = useState<number>()
+  const [amount, setAmount] = useState<number>(1)
+  const [amountInFromCurrency, setAmountInFromCurrency] = useState<boolean>(true)
   
   useEffect(()=>{
     fetch(BASE_URL)
     .then(res=>res.json())
     .then(data => {
-      setCurrencyOptions([...Object.keys(data.rates)])
-          // ...Object.keys(data.rates)
+      const firstCurrency = Object.keys(data.rates)[0];
+      setCurrencyOptions(Object.keys(data.rates));
 
-      setFromCurrency(data.base)
-      setToCurrency(Object.keys(data.rates)[0])
+      setFromCurrency(data.base);
+      setToCurrency(firstCurrency);
+      setExchangeRate(data.rates[firstCurrency]);
     })
   }, [])
+
+
+
+  let fromAmount:number, toAmount:number
+  if(amountInFromCurrency){
+    fromAmount = amount
+    toAmount = fromAmount * exchangeRate
+  } else {
+    toAmount = amount
+    fromAmount = toAmount / exchangeRate
+  }
+
+  const handleFromAmountChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setAmount(Number(e.target.value));
+    setAmountInFromCurrency(true);
+  }
+
+  const handleToAmountChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setAmount(Number(e.target.value));
+    setAmountInFromCurrency(false);
+  }
+
+  useEffect(()=>{
+    fetch(`${BASE_URL}?base=${fromCurrency}&symbols=${toCurrency}`)
+    // fetch(`${BASE_URL}?base=${fromCurrency}&symbols=${toCurrency}`)
+    .then(res => res.json())
+    .then(data => setExchangeRate(data.rates[toCurrency]))
+  }, [fromCurrency, toCurrency])
 
   return (
     <div className="flex justify-center items-center min-h-screen text-center">
       <div>
       <h1 className="text-2xl mb-5">Convert</h1>
-      <CurrencyRow currencyOptions={currencyOptions} selectedCurrency={fromCurrency} onChangeCurrency={(e: React.ChangeEvent<HTMLSelectElement>)=>setFromCurrency(e.target.value)}/>
+      <CurrencyRow 
+      currencyOptions = {currencyOptions} 
+      selectedCurrency = {fromCurrency} 
+      onChangeCurrency = {(e: React.ChangeEvent<HTMLSelectElement>)=>setFromCurrency(e.target.value)}
+      onChangeAmount = {handleFromAmountChange}
+      amount = {fromAmount}
+      />
       <div>=</div>
-      <CurrencyRow currencyOptions={currencyOptions} selectedCurrency={toCurrency} onChangeCurrency={(e: React.ChangeEvent<HTMLSelectElement>)=>setToCurrency(e.target.value)}/>
+      <CurrencyRow 
+      currencyOptions = {currencyOptions} 
+      selectedCurrency = {toCurrency} 
+      onChangeCurrency = {(e: React.ChangeEvent<HTMLSelectElement>)=>setToCurrency(e.target.value)}
+      onChangeAmount = {handleToAmountChange}
+      amount = {toAmount}
+      />
       </div>
     </div>
   );
--- src/CurrencyRow.tsx
+++ src/CurrencyRow.tsx
@@ -3,26 +3,30 @@ import { FC } from "react"
 type Props = {
   currencyOptions: string[],
   selectedCurrency: string,
-  onChangeCurrency(e:React.ChangeEvent<HTMLSelectElement>): void
+  onChangeCurrency(e:React.ChangeEvent<HTMLSelectElement>): void,
+  amount: number,
+  onChangeAmount(e:React.ChangeEvent<HTMLInputElement>): void
 }
 
 
 
-function CurrencyRow ({currencyOptions, selectedCurrency, onChangeCurrency}: Props) {
+function CurrencyRow ({currencyOptions, selectedCurrency, onChangeCurrency, amount, onChangeAmount}: Props) {
 
   return (
     <div className="flex gap-5">
       <input 
-      type="number" 
-      placeholder="Type a number" 
-      className="input input-bordered input-warning w-full max-w-xs flex-none" /> 
+      type = "number" 
+      placeholder = "Type a number" 
+      className = "input input-bordered input-warning w-full max-w-xs flex-none" 
+      value = {amount}
+      onChange={onChangeAmount}/> 
 
       <select 
-      value= {selectedCurrency}
-      onChange={onChangeCurrency}
-      className="select select-warning w-full max-w-xs flex-1 w-32"> 
+      value = {selectedCurrency}
+      onChange = {onChangeCurrency}
+      className = "select select-warning w-full max-w-xs flex-1 w-32"> 
       {currencyOptions.map((option: string)=>
-      <option key={option} value={option}>{option}</option>
+      <option key = {option} value = {option}>{option}</option>
       )}
 
       </select>
